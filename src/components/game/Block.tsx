a6696cbc37d31aa4303b2ede34f130bf45b210b6
n1md7 <b.kodua@gmail.com>
2024-10-16 02:10:44 +0300

    feat: new model versions added

--- src/components/game/Block.tsx
+++ src/components/game/Block.tsx
@@ -1,38 +1,42 @@
 import { BufferGeometry, Material, MeshStandardMaterial } from 'three';
 import { useRef } from 'react';
+import { Vector3 } from '@react-three/fiber';
 
 type Props = {
   geometry: BufferGeometry;
-  position: [number, number, number];
+  position: Vector3;
   castShadow?: boolean;
   receiveShadow?: boolean;
   name?: string;
   material?: Material | Material[];
+  scale?: Vector3;
 };
 
 const defaultColor = 'gray';
 
-export function Block({ geometry, position, name, castShadow = false, receiveShadow = false }: Props) {
+export function Block({ geometry, position, name, scale }: Props) {
   const colorRef = useRef<MeshStandardMaterial>(null!);
   const colorMap = useRef<Record<number, string>>({
-    10: 'red', // Top face id
-    11: 'green', // Left face id
-    12: 'blue', // Bottom face id
-    13: 'purple', // Right face id
+    11: 'red', // Top face id
+    13: 'green', // Left face id
+    15: 'blue', // Bottom face id
+    17: 'purple', // Right face id
   });
 
   return (
     <mesh
       name={name}
-      castShadow={castShadow}
-      receiveShadow={receiveShadow}
+      castShadow={true}
+      receiveShadow={true}
       onPointerMove={({ faceIndex }) => {
+        console.info(faceIndex);
         if (!faceIndex) return;
         colorRef.current.color.setColorName(colorMap.current[faceIndex] || defaultColor);
       }}
       onPointerOut={() => colorRef.current.color.setColorName(defaultColor)}
       geometry={geometry}
       position={position}
+      scale={scale}
     >
       <meshStandardMaterial ref={colorRef} color={defaultColor} />
     </mesh>
