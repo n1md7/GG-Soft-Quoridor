28ba4b6c1c989208b72c2a1e9dffb5a41e735a91
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2024-10-19 03:01:59 +0800

    added initial responsiveness, progress bar design, initial button design

--- src/views/InitialView.tsx
+++ src/views/InitialView.tsx
@@ -15,16 +15,18 @@ export function InitialView({ next }: Props) {
       <div className="logo">
         <img src={LogoImg} alt="Quoridor 3D" />
       </div>
-      <p className="description min-w-48 text-red-500">
-        The game of Quoridor is a strategy game where the goal is to reach the opposite side of the board. The catch is
-        that you can place walls to block your opponent. The first player to reach the opposite side wins.
-      </p>
-      <ProgressBar />
-      <Show when={loaded === total}>
-        <button onClick={next} className="play-button">
-          Play
-        </button>
-      </Show>
+      <div className="container xs:w-5/6 md:w-2/3 lg:w-2/4 xl:w-3/12">
+        <p className="description text-sm">
+          The game of Quoridor is a strategy game where the goal is to reach the opposite side of the board. The catch
+          is that you can place walls to block your opponent. The first player to reach the opposite side wins.
+        </p>
+        <ProgressBar />
+        <Show when={loaded === total}>
+          <button onClick={next} className="play-button">
+            ENTER
+          </button>
+        </Show>
+      </div>
     </div>
   );
 }
