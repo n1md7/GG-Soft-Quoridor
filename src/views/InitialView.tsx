77b92cf5eca7c36d22e31d2ab793f9c2bcf35575
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2024-10-25 03:17:10 +0800

    Initial lobby page (#23)
    * resolved conflicts
    
    * added initial responsiveness, progress bar design, initial button design
    
    * remove transition for progress bar

--- src/views/InitialView.tsx
+++ src/views/InitialView.tsx
@@ -1,5 +1,5 @@
 import '@styles/initial-view.scss';
-import LogoImg from '@assets/react.svg';
+import LogoImg from '@assets/quoridor-logo.png';
 import { useProgress } from '@react-three/drei';
 import { ProgressBar } from '@src/components/ui/ProgressBar.tsx';
 import { Show } from '@src/components/utils/Show.tsx';
@@ -12,20 +12,21 @@ export function InitialView({ next }: Props) {
 
   return (
     <div className="initial-view">
-      <h1 className="title">Quoridor 3D</h1>
       <div className="logo">
         <img src={LogoImg} alt="Quoridor 3D" />
       </div>
-      <p className="description min-w-48 text-red-500">
-        The game of Quoridor is a strategy game where the goal is to reach the opposite side of the board. The catch is
-        that you can place walls to block your opponent. The first player to reach the opposite side wins.
-      </p>
-      <ProgressBar />
-      <Show when={loaded === total}>
-        <button onClick={next} className="play-button">
-          Play
-        </button>
-      </Show>
+      <div className="container xs:w-5/6 md:w-2/3 lg:w-2/4 xl:w-3/12">
+        <p className="description text-sm">
+          The game of Quoridor is a strategy game where the goal is to reach the opposite side of the board. The catch
+          is that you can place walls to block your opponent. The first player to reach the opposite side wins.
+        </p>
+        <ProgressBar />
+        <Show when={loaded === total}>
+          <button onClick={next} className="play-button">
+            ENTER
+          </button>
+        </Show>
+      </div>
     </div>
   );
 }
