b4142a012560dcc6093864644f4f81abfb7de8b3
n1md7 <n1md7@users.noreply.github.com>
2024-11-08 22:06:17 +0000

    deploy: e7d3c4dbb0112286186f90de23f532c6b5d3c936

--- src/views/LobbyView.tsx
+++ src/views/LobbyView.tsx
@@ -6,12 +6,55 @@ type Props = {
 export function LobbyView({ next }: Props) {
   return (
     <div className="lobby-view">
-      <h1 className="title">Lobby</h1>
-      <p className="description">
-        This is the lobby where you can see the list of games available to join or create a new game.
-      </p>
+      <div className="main-lobby-container">
+        {/* game settings */}
+        <div className="main-container">
+          <div className="lens-container top">
+            <div className="lens"></div>
+            <div className="eclipse"></div>
+          </div>
+          <div className="wrapper-outline">
+            <div className="wrapper">
+              <div className="wrapper-border"></div>
+            </div>
+          </div>
+          <div className="main-trapezoid">
+            <div className="trapezoid settings"></div>
+          </div>
+          <div className="sides-container">
+            <div className="side"></div>
+            <div className="side flip"></div>
+          </div>
+          <div className="parallelograms">
+            <div className="parallelogram"></div>
+            <div className="parallelogram flip"></div>
+          </div>
+          <div className="lens-container bottom">
+            <div className="lens flip"></div>
+            <div className="eclipse"></div>
+          </div>
+        </div>
+
+        {/* leaderboard */}
+        <div className="main-container">
+          <div className="wrapper-outline">
+            <div className="wrapper">
+              <div className="wrapper-border"></div>
+            </div>
+          </div>
+
+          <div className="main-trapezoid for-leaderboard">
+            <div className="trapezoid leaderboard"></div>
+            <div className="minis">
+              <div className="mini-trapezoid"></div>
+              <div className="mini-trapezoid"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <button onClick={next} className="play-button">
-        Play
+        ENTER
       </button>
     </div>
   );
