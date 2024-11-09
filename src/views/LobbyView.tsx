ca7e8c47c54c9f95308a1a23183fc2b09ac893ce
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2024-11-10 02:50:17 +0800

    wip: applied changes for leaderboard.

--- src/views/LobbyView.tsx
+++ src/views/LobbyView.tsx
@@ -19,7 +19,9 @@ export function LobbyView({ next }: Props) {
             </div>
           </div>
           <div className="main-trapezoid">
-            <div className="trapezoid settings"></div>
+            <div className="trapezoid settings">
+              <span>Settings</span>
+            </div>
           </div>
           <div className="sides-container">
             <div className="side"></div>
@@ -37,6 +39,10 @@ export function LobbyView({ next }: Props) {
 
         {/* leaderboard */}
         <div className="main-container">
+          <div className="lens-container top">
+            <div className="lens"></div>
+            <div className="eclipse"></div>
+          </div>
           <div className="wrapper-outline">
             <div className="wrapper">
               <div className="wrapper-border"></div>
@@ -44,11 +50,25 @@ export function LobbyView({ next }: Props) {
           </div>
 
           <div className="main-trapezoid for-leaderboard">
-            <div className="trapezoid leaderboard"></div>
+            <div className="trapezoid leaderboard">
+              <span>Leaderboard</span>
+            </div>
             <div className="minis">
               <div className="mini-trapezoid"></div>
               <div className="mini-trapezoid"></div>
             </div>
+          </div>
+          <div className="sides-container">
+            <div className="side"></div>
+            <div className="side flip"></div>
+          </div>
+          <div className="parallelograms">
+            <div className="parallelogram"></div>
+            <div className="parallelogram flip"></div>
+          </div>
+          <div className="lens-container bottom">
+            <div className="lens flip"></div>
+            <div className="eclipse"></div>
           </div>
         </div>
       </div>
