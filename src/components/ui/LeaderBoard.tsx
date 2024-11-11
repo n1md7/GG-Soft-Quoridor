acd8f4b1115f535ded63c02f91e6b30218dbd64a
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2024-11-12 04:10:06 +0800

    create form container for settings board.

--- src/components/ui/LeaderBoard.tsx
+++ src/components/ui/LeaderBoard.tsx
@@ -1,4 +1,4 @@
-import { Parallelogram } from '@src/components/ui/Parallelogram';
+import { BoardElements } from '@src/components/ui/BoardElements';
 
 export function LeaderBoard() {
   return (
@@ -12,8 +12,13 @@ export function LeaderBoard() {
           <div className="mini-trapezoid"></div>
         </div>
       </div>
+      <div className="wrapper-outline">
+        <div className="wrapper">
+          <div className="wrapper-border"></div>
+        </div>
+      </div>
 
-      <Parallelogram />
+      <BoardElements />
     </div>
   );
 }
--- src/components/ui/SettingsBoard.tsx
+++ src/components/ui/SettingsBoard.tsx
@@ -1,15 +1,87 @@
-import { Parallelogram } from '@src/components/ui/Parallelogram';
+import React, { useState } from 'react';
+import { BoardElements } from '@src/components/ui/BoardElements';
+import { Parallelogram } from './Parallelogram';
 
 export function SettingsBoard() {
+  const [inputValue, setInputValue] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleRadioChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
+
   return (
-    <div className="main-container">
+    <div className="main-container setting-board">
       <div className="main-trapezoid">
         <div className="trapezoid settings">
           <span className="header">Settings</span>
         </div>
       </div>
+      <div className="wrapper-outline">
+        <div className="wrapper">
+          <div className="wrapper-border">
+            <div className="input-container">
+              <Parallelogram />
+              {/* Input Text for Player Name */}
+              <label htmlFor="playerName">Your name:</label>
+              <input
+                type="text"
+                id="playerName"
+                value={inputValue}
+                onChange={handleInputChange}
+                className="input-name"
+              />
 
-      <Parallelogram />
+              <div className="input-container">
+                {/* Radio Buttons */}
+                <label htmlFor="">Difficulty:</label>
+
+                <label>
+                  <input
+                    type="radio"
+                    name="mode"
+                    value="easy"
+                    checked={selectedOption === 'easy'}
+                    onChange={handleRadioChange}
+                    className="hidden"
+                  />
+                  <div className="input-mode">Easy</div>
+                </label>
+
+                <label>
+                  <input
+                    type="radio"
+                    name="mode"
+                    value="normal"
+                    checked={selectedOption === 'normal'}
+                    onChange={handleRadioChange}
+                    className="hidden"
+                  />
+                  <div className="input-mode">Normal</div>
+                </label>
+
+                <label>
+                  <input
+                    type="radio"
+                    name="mode"
+                    value="intense"
+                    checked={selectedOption === 'intense'}
+                    onChange={handleRadioChange}
+                    className="hidden"
+                  />
+                  <div className="input-mode">Intense</div>
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <BoardElements />
     </div>
   );
 }
