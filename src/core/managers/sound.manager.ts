7c6b69d42226617f6d938272a250bd66f36c291a
n1md7 <b.kodua@gmail.com>
2025-09-08 00:32:09 +0300

    fix: abs path

--- src/core/managers/sound.manager.ts
+++ src/core/managers/sound.manager.ts
@@ -1,9 +1,9 @@
 import { Sound } from '@src/core/classes/sound.class.ts';
 
 export class SoundManager {
-  readonly background = new Sound({ src: '/sounds/background.mp3', loop: true, volume: 0.01 });
-  readonly pawnMove = new Sound({ src: '/sounds/pawn-move.mp3', volume: 0.5, endAt: 600 });
-  readonly playerWin = new Sound({ src: '/sounds/player-win.mp3', volume: 0.5 });
-  readonly playerError = new Sound({ src: '/sounds/player-error.mp3', volume: 0.3 });
-  readonly wallPlacement = new Sound({ src: '/sounds/wall-place.mp3', volume: 0.5 });
+  readonly background = new Sound({ src: './sounds/background.mp3', loop: true, volume: 0.01 });
+  readonly pawnMove = new Sound({ src: './sounds/pawn-move.mp3', volume: 0.5, endAt: 600 });
+  readonly playerWin = new Sound({ src: './sounds/player-win.mp3', volume: 0.5 });
+  readonly playerError = new Sound({ src: './sounds/player-error.mp3', volume: 0.3 });
+  readonly wallPlacement = new Sound({ src: './sounds/wall-place.mp3', volume: 0.5 });
 }
