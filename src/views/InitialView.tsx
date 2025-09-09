23baa2f9792ebc2cefc440831a4a4ab131d8f227
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2025-09-09 17:41:25 +0300

    back button for sound settings

--- src/views/InitialView.tsx
+++ src/views/InitialView.tsx
@@ -2,55 +2,35 @@ import LogoImg from '@assets/quoridor-logo.png';
 import { useProgress } from '@react-three/drei';
 import { ProgressBar } from '@src/components/ui/ProgressBar.tsx';
 import { Show } from '@src/components/utils/Show.tsx';
-import { useState } from 'react';
-import { GameSettings } from '@src/components/ui/GameSettings.tsx';
 
 type Props = {
   next: () => void;
 };
 export function InitialView({ next }: Readonly<Props>) {
   const { loaded, total } = useProgress();
-  const [soundSettings, setSoundSettings] = useState(false);
 
   return (
     <div className="initial-view">
-      <div className="game-settings-container">
-        <div className="settings-icon" onClick={() => setSoundSettings(true)}>
-          <div className="icon game-settings"></div>
-        </div>
-        <div className="settings-icon">
-          <div className="icon volume-settings"></div>
+      <div className="main-view-container">
+        <div className="container">
+          <div className="logo">
+            <img src={LogoImg} alt="Quoridor 3D" />
+          </div>
+
+          <p className="description">
+            The game of Quoridor is a strategy game where the goal is to reach the opposite side of the board. The catch
+            is that you can place walls to block your opponent. The first player to reach the opposite side wins.
+          </p>
+          <ProgressBar />
         </div>
       </div>
-      <Show when={soundSettings}>
-        <div className="lobby-view">
-          <GameSettings />
-        </div>
-      </Show>
-
-      <Show when={!soundSettings}>
-        <div className="main-view-container">
-          <div className="container">
-            <div className="logo">
-              <img src={LogoImg} alt="Quoridor 3D" />
-            </div>
-
-            <p className="description">
-              The game of Quoridor is a strategy game where the goal is to reach the opposite side of the board. The
-              catch is that you can place walls to block your opponent. The first player to reach the opposite side
-              wins.
-            </p>
-            <ProgressBar />
-          </div>
-        </div>
-        <div className="button-grp">
-          <Show when={loaded === total}>
-            <button onClick={next} className="play-button">
-              ENTER
-            </button>
-          </Show>
-        </div>
-      </Show>
+      <div className="button-grp">
+        <Show when={loaded === total}>
+          <button onClick={next} className="play-button">
+            ENTER
+          </button>
+        </Show>
+      </div>
     </div>
   );
 }
