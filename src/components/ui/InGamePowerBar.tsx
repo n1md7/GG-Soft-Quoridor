8ec8044ca143e04ad7486f0f225debfbfcca0126
n1md7 <n1md7@users.noreply.github.com>
2025-10-05 15:38:18 +0000

    deploy: 7a73abb52e538cf1c2e81b093588937a6d221be5

---
+++ src/components/ui/InGamePowerBar.tsx
@@ -0,0 +1,190 @@
+import { useGame } from '@src/components/hooks/useGame.ts';
+import { Show } from '@src/components/utils/Show.tsx';
+import { PowerEnum } from '@src/core/enums/power.enum.ts';
+import { useCallback, useState, useEffect, useMemo } from 'react';
+import classNames from 'classnames';
+
+type StateType = 'is-usable' | 'ad-available' | 'disabled';
+type PowerProps = {
+  key: PowerEnum;
+  name: string;
+  description: string;
+  placeholder: string;
+  state: StateType;
+};
+
+export function InGamePowerBar() {
+  const { inventory } = useGame();
+
+  const powers: PowerProps[] = useMemo(
+    () =>
+      [
+        {
+          key: PowerEnum.ExtraWall,
+          name: 'Extra Wall',
+          description: 'Get an additional wall to place.',
+          placeholder: '🧱',
+          state: 'disabled' as StateType,
+        },
+        {
+          key: PowerEnum.ShortestPath,
+          name: 'Show Path',
+          description: 'Show the shortest path to your goal.',
+          placeholder: '🗺️',
+          state: 'disabled' as StateType,
+        },
+        {
+          key: PowerEnum.BlockMove,
+          name: 'Block Move',
+          description: 'Block opponent from moving. This makes opponent to skip one move.',
+          placeholder: '🚫',
+          state: 'disabled' as StateType,
+        },
+        {
+          key: PowerEnum.Undo,
+          name: 'Undo',
+          description: 'Undo your last move you and player made.',
+          placeholder: '↶',
+          state: 'disabled' as StateType,
+        },
+      ].map((power) => {
+        const item = inventory.getItems().find((item) => item.getKey() === power.key);
+
+        if (item) {
+          if (item.isUsable()) power.state = 'is-usable';
+          else if (item.canWatchAd()) power.state = 'ad-available';
+        }
+
+        return power;
+      }),
+    [inventory],
+  );
+
+  return (
+    <div
+      className="power-bar"
+      style={{
+        pointerEvents: 'auto',
+        userSelect: 'none',
+      }}
+    >
+      <div className="relative">
+        <div className="container">
+          <div className="top-line" />
+          <div className="bottom-line" />
+          {powers.map((power, index) => (
+            <div key={power.key} className="item">
+              <PowerButton power={power} />
+              <Show when={index < powers.length - 1}>
+                <div className="vertical-divider" />
+              </Show>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+type PowerButtonProps = {
+  power: PowerProps;
+};
+
+function PowerButton({ power }: PowerButtonProps) {
+  const { inventory, market, powers } = useGame();
+
+  const [state, setState] = useState<StateType>(power.state);
+
+  const buttonClass = useMemo(() => {
+    return classNames('button', {
+      ['ad-available']: state === 'ad-available',
+      ['disabled']: state === 'disabled',
+    });
+  }, [state]);
+
+  const handleClick = useCallback(() => {
+    switch (state) {
+      case 'is-usable':
+        powers.use(power.key);
+        break;
+      case 'ad-available':
+        inventory.unlockViaAd(power.key);
+        break;
+    }
+  }, [inventory, power.key, powers, state]);
+
+  const onPurchaseAction = useCallback(
+    (key: PowerEnum) => {
+      if (power.key === key) {
+        setState('is-usable');
+      }
+    },
+    [power],
+  );
+
+  const onInventoryUseAction = useCallback(
+    (key: PowerEnum) => {
+      if (power.key === key) {
+        setState('disabled');
+      }
+    },
+    [power.key],
+  );
+
+  const onInventoryUnlockAction = useCallback(
+    (key: PowerEnum) => {
+      if (power.key === key) {
+        setState('is-usable');
+      }
+    },
+    [power],
+  );
+
+  useEffect(() => {
+    market.on('purchase', onPurchaseAction);
+    inventory.on('use', onInventoryUseAction);
+    inventory.on('unlock', onInventoryUnlockAction);
+
+    return () => {
+      market.off('purchase', onPurchaseAction);
+      inventory.off('use', onInventoryUseAction);
+      inventory.off('unlock', onInventoryUnlockAction);
+    };
+  }, [inventory, market, onInventoryUnlockAction, onInventoryUseAction, onPurchaseAction]);
+
+  return (
+    <div className="power-container">
+      <button onClick={handleClick} disabled={state === 'disabled'} className={buttonClass}>
+        <div className="icon">{power.placeholder}</div>
+      </button>
+
+      <Show when={state === 'ad-available'}>
+        <button onClick={handleClick} className="ad-overlay">
+          <div className="play-icon">▶</div>
+        </button>
+      </Show>
+
+      <div className="tooltip">
+        <div className="title">{power.name}</div>
+        <div className="description">
+          {power.description}
+
+          <Show when={state === 'is-usable'}>
+            <div className="horizontal-divider" />
+            <div className="hint-text">You own this power! You can activate it by clicking the button above.</div>
+          </Show>
+          <Show when={state === 'ad-available'}>
+            <div className="horizontal-divider" />
+            <div className="ad-message">
+              You can unlock this power by watching an ad. Click the play button on the power icon to watch an ad.
+            </div>
+          </Show>
+          <Show when={state === 'disabled'}>
+            <div className="horizontal-divider" />
+            <div className="hint-text">You can only use each power once per game.</div>
+          </Show>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/core/managers/inventory.manager.ts
+++ src/core/managers/inventory.manager.ts
@@ -1,19 +1,29 @@
 import { InventoryItem } from '@src/core/classes/inventory-item.class.ts';
 import { PowerEnum } from '@src/core/enums/power.enum.ts';
 import { Game } from '@src/core/game.class.ts';
+import { TinyEmitter } from 'tiny-emitter';
+
+export type Events = 'use' | 'purchase' | 'unlock';
+type ItemUseFn = (power: PowerEnum) => void;
+type ItemUnlockFn = (power: PowerEnum) => void;
+type ItemPurchaseFn = (power: PowerEnum, remainingCoins: number) => void;
+export type Fns = ItemUseFn | ItemPurchaseFn | ItemUnlockFn;
 
 export class InventoryManager {
   private static instance: InventoryManager;
 
   private readonly powers: Record<PowerEnum, InventoryItem>;
+  private readonly events: TinyEmitter;
 
   private constructor(private readonly game: Game) {
+    this.events = new TinyEmitter();
     this.powers = {
       [PowerEnum.ShortestPath]: new InventoryItem({ key: PowerEnum.ShortestPath }),
       [PowerEnum.ExtraWall]: new InventoryItem({ key: PowerEnum.ExtraWall }),
       [PowerEnum.BlockMove]: new InventoryItem({ key: PowerEnum.BlockMove }),
       [PowerEnum.Undo]: new InventoryItem({ key: PowerEnum.Undo }),
     };
+    this.restore();
   }
 
   static getInstance(game: Game) {
@@ -22,6 +32,28 @@ export class InventoryManager {
     }
 
     return InventoryManager.instance;
+  }
+  on(event: 'use', fn: ItemUseFn): void;
+  on(event: 'unlock', fn: ItemUnlockFn): void;
+  on(event: 'purchase', fn: ItemPurchaseFn): void;
+  on(event: Events, callback: Fns) {
+    this.events.on(event, callback);
+  }
+
+  off(event: 'use', fn: ItemUseFn): void;
+  off(event: 'unlock', fn: ItemUnlockFn): void;
+  off(event: 'purchase', fn: ItemPurchaseFn): void;
+  off(event: Events, callback: Fns) {
+    this.events.off(event, callback);
+  }
+
+  emit(event: 'use', power: PowerEnum): boolean;
+  emit(event: 'unlock', power: PowerEnum): boolean;
+  emit(event: 'purchase', power: PowerEnum, remainingCoins: number): boolean;
+  emit(event: Events, ...args: unknown[]) {
+    this.events.emit(event, ...args);
+
+    return true;
   }
 
   restore() {
@@ -42,6 +74,10 @@ export class InventoryManager {
     return this.powers[power].isUsable();
   }
 
+  canWatchAd(power: PowerEnum): boolean {
+    return this.powers[power].canWatchAd();
+  }
+
   use(power: PowerEnum): boolean {
     if (!this.powers[power].use()) return false;
 
@@ -51,15 +87,22 @@ export class InventoryManager {
       ownedPowers: ownedPowers.filter((p) => p !== power),
     });
 
-    return true;
+    return this.emit('use', power);
   }
 
   unlockViaAd(power: PowerEnum): boolean {
-    return this.powers[power].unlockViaAd();
+    if (!this.powers[power].unlockViaAd()) return false;
+
+    return this.emit('unlock', power);
   }
 
   unlockViaPurchase(power: PowerEnum): boolean {
-    return this.powers[power].unlockViaPurchase();
+    if (!this.powers[power].unlockViaPurchase()) return false;
+
+    const name = this.game.player.getName();
+    const { coins } = this.game.storage.getByName(name);
+
+    return this.emit('purchase', power, coins);
   }
 
   reset() {
