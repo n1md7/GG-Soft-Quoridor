21ce773b055cda0e5ca8a9e7970e83250001cf37
n1md7 <b.kodua@gmail.com>
2026-02-12 20:38:34 +0200

    feat: credits

--- src/views/LobbyView.tsx
+++ src/views/LobbyView.tsx
@@ -1,5 +1,6 @@
 import { LobbyButtons } from '@src/components/ui/LobbyButtons';
 import { LobbyContent } from '@src/components/ui/LobbyContent';
+import { CreditsModal } from '@src/components/ui/CreditsModal';
 import { memo, useCallback, useState } from 'react';
 
 type Props = {
@@ -8,9 +9,14 @@ type Props = {
 
 export const LobbyView = memo(function LobbyView({ next }: Props) {
   const [showLeaderboard, setShowLeaderboard] = useState(false);
+  const [showCredits, setShowCredits] = useState(false);
 
   const handleToggleLeaderboard = useCallback(() => {
     setShowLeaderboard((prev) => !prev);
+  }, []);
+
+  const handleToggleCredits = useCallback(() => {
+    setShowCredits((prev) => !prev);
   }, []);
 
   return (
@@ -21,6 +27,12 @@ export const LobbyView = memo(function LobbyView({ next }: Props) {
         onToggleLeaderboard={handleToggleLeaderboard}
         onStartGame={next}
       />
+      {!showLeaderboard && (
+        <button className="lobby-credits-link" onClick={handleToggleCredits}>
+          Credits
+        </button>
+      )}
+      <CreditsModal isOpen={showCredits} onClose={handleToggleCredits} />
     </div>
   );
 });
