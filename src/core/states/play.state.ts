859247a641569422e31788c716f8889f3a778e3c
n1md7 <b.kodua@gmail.com>
2026-02-15 20:57:00 +0200

    feat: disable power bar

--- src/core/states/play.state.ts
+++ src/core/states/play.state.ts
@@ -12,12 +12,14 @@ export class PlayState extends GameState {
     this.game.timer.start();
     this.game.sounds.bg.play();
     this.game.platform.startGame().catch();
+    this.game.powers.events.emit('disable', { disable: false });
   }
 
   override deactivate() {
     this.game.timer.stop();
     this.game.sounds.bg.stop();
     this.game.platform.stopGame().catch();
+    this.game.powers.events.emit('disable', { disable: true });
 
     super.deactivate();
   }
